f944c9a22cfdcddc73e51bd33ce3a71f3f130e4f
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-11-15 09:50:45 +0900

    테스트 코드 완성

--- users/tests.py
+++ users/tests.py
@@ -12,4 +12,4 @@ class UserSignupTest(APITestCase):
         }
         response = self.client.post(url, user_data)
         print(response.data)
-        self.assertEqual(response.data, {"message":"가입 완료!!"})
+        self.assertEqual(response.data["message"], "가입 완료!!")
